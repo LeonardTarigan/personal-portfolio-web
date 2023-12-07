dd94cf3a74f84ab217ab405785a26854d4d16ff3
LeonardTarigan <leontrg32@gmail.com>
2023-12-07 20:11:14 +0700

    disabling submit button while sending email

--- src/components/collab.tsx
+++ src/components/collab.tsx
@@ -1,22 +1,28 @@
 import { fadeRightVariant } from "@/utils/animation";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import toast from "react-hot-toast";
 
 function Collab() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const formRef = useRef<HTMLFormElement>(null);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
+    setIsLoading(true);
+
     toast.promise(
-      emailjs.sendForm(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ?? "",
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ?? "",
-        formRef.current ?? "",
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_API_KEY,
-      ),
+      emailjs
+        .sendForm(
+          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ?? "",
+          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ?? "",
+          formRef.current ?? "",
+          process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_API_KEY,
+        )
+        .finally(() => setIsLoading(false)),
       {
         loading: "Sending message...",
         success: "Message sent succesfully",
@@ -71,8 +77,11 @@ function Collab() {
               className="w-full rounded-md bg-zinc-800 p-2 text-white placeholder:font-medium placeholder:text-zinc-500"
             />
           </div>
-          <button className="mt-10 w-full rounded-md border-2 border-white bg-white px-5 py-2 font-bold text-black transition-all duration-150 hover:bg-transparent hover:text-white active:translate-y-2">
-            Send Email
+          <button
+            disabled={isLoading}
+            className="mt-10 w-full rounded-md border-2 border-white bg-white px-5 py-2 font-bold text-black transition-all duration-150 hover:bg-transparent hover:text-white active:translate-y-2 disabled:cursor-not-allowed disabled:border-zinc-700 disabled:bg-zinc-800 disabled:text-zinc-500 disabled:active:translate-y-0"
+          >
+            {isLoading ? "Sending..." : "Send Email"}
           </button>
         </form>
       </motion.div>
